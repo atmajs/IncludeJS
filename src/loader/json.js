f769c63e73a25b098766e49eadb83e66e09a6815
Alexander Kit <source.kit@gmail.com>
2013-06-17 12:11:39 +0200

    [++] json loader

---
+++ src/loader/json.js
@@ -0,0 +1,12 @@
+	
+var JSONParser = {
+	process: function(source, res){
+		try {
+			return JSON.parse(source);
+		} catch(error) {
+			console.error(error, source);
+			return null;
+		}
+	}
+};
+
