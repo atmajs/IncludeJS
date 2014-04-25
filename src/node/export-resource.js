6605e96d95432927b59a73023adef45eab1ff6a5
Alex Kit <source.kit@gmail.com>
2014-04-25 17:04:54 +0200

    fix dbl var declaration

--- src/node/export-resource.js
+++ src/node/export-resource.js
@@ -78,8 +78,7 @@
                     
                     var PATH = process.env.PATH || process.env.path,
                         delimiter = require('path').delimiter,
-                        parts = PATH.split(delimiter),
-                        npmPath;
+                        parts = PATH.split(delimiter);
                         
                     var i = parts.length,
                         rgx = /([\\\/]npm[\\\/])|([\\\/]npm$)/gi;
