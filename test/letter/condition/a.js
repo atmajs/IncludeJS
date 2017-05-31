38bc34d81253a94ff52906f3a854785507b0bcec
Alex Kit <source.kit@gmail.com>
2017-05-31 23:22:35 +0200

    v0.12.48

--- test/letter/condition/a.js
+++
@@ -1,8 +0,0 @@
-include.js('b.js::Letter').done(function(resp){
-
-	eq('b', resp.Letter, 'Parameter should be equal to returned response');
-	
-	assert.notEqual(include.url.indexOf('/a.js'), -1);
-	
-	include.exports = resp.Letter;
-})
